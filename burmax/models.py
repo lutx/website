caa9a76dea590ecb0be2bb0feb580644109b527a
lukaszu <lukaszburmax@gmail.com>
2018-03-30 12:31:33 +0200

    Creates  Models

--- burmax/models.py
+++ burmax/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Shapes(models.Model):
+    name = models.CharField(max_length=50)
+    info_shapes = models.CharField(max_length=500)
+    picture_shapes = models.CharField(max_length=1000)
+
+
+class CutsTypes(models.Model):
+    shapes = models.ForeignKey(Shapes, on_delete=models.CASCADE)
+    types_name = models.CharField(max_length=50)
+    info_types = models.CharField(max_length=500)
+    picture_cuts = models.CharField(max_length=1000)
+
+
+class ShankSize(models.Model):
+    cutsTypes = models.ForeignKey(CutsTypes, on_delete=models.CASCADE)
+    shank_size_name = models.CharField(max_length=50)
+
+
+class HeadSize(models.Model):
+    shankSize = models.ForeignKey(ShankSize, on_delete=models.CASCADE)
+    head_size_name = models.CharField(max_length=50)
